8c6b920bc95f8032c3c808f568d545e34682d030
Joe Nellis <joe@jnellis.net>
2014-12-17 17:56:05 -0800

    NextFitPackingPolicy and test

--- src/main/java/net/jnellis/binpack/packing/NextFitPackingPolicy.java
+++ src/main/java/net/jnellis/binpack/packing/NextFitPackingPolicy.java
@@ -1,6 +1,14 @@
 /*
  * NextFitPackingPolicy.java
  *
+ * Copyright (c) 2014. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
+/*
+ * NextFitPackingPolicy.java
+ *
  * Created on September 3, 2006, 4:06 PM
  *
  * To change this template, choose Tools | Template Manager
@@ -18,22 +26,30 @@ import java.util.List;
  * @author Joe Nellis
  */
 public class NextFitPackingPolicy implements LinearPackingPolicy {
+  /**
+   * Attempts to place one <code>piece</code> into bins of
+   * <code>existingBins</code>
+   * sizes first then uses <code>availableCapacities</code> bin sizes when it
+   * needs to
+   * fill a new bin.
+   *
+   * @param piece               The pieces that need to be packed.
+   * @param existingBins        Existing bins that pieces will be packed
+   *                            into first.
+   * @param availableCapacities Available bin sizes that we can create.
+   * @return Returns a stream of packed bins.
+   */
   @Override
-  public List<Bin<Double>> pack(Double piece, List<Bin<Double>>
-      existingBins, List<Double> availableCapacities) {
-    return null;
-//    pieces.forEach(piece -> {
-//      Bin<Double> lastBin = null;
-//      ListIterator<Bin<Double>> iterator = bins.listIterator();
-//      if (iterator.hasPrevious()) {
-//        lastBin = iterator.previous();
-//      }
-//      if(lastBin == null || !lastBin.canFit(piece)) {
-//        lastBin = createNewBin(bins, capacity);
-//      }
-//      place(piece,lastBin);
-//    });
-  }
+  public List<Bin<Double>> pack(Double piece, List<Bin<Double>> existingBins,
+                                List<Double> availableCapacities) {
 
+    Bin<Double> lastBin = existingBins.stream()
+        .max((a, b) -> -1) // aka findLast
+        .filter(bin -> bin.canFit(piece))
+        .orElseGet(() -> addNewBin(existingBins, piece, availableCapacities));
+
+    lastBin.add(piece);
+    return existingBins;
+  }
 
 }
