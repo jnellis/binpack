21be42b264446e241fa2e30e0521b89dc2c95398
Joe Nellis <joe@jnellis.net>
2015-01-06 07:07:30 -0800

    WorstFitPackingPolicy and test

--- src/main/java/net/jnellis/binpack/packing/WorstFitPackingPolicy.java
+++ src/main/java/net/jnellis/binpack/packing/WorstFitPackingPolicy.java
@@ -1,6 +1,14 @@
 /*
  * WorstFitPackingPolicy.java
  *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
+/*
+ * WorstFitPackingPolicy.java
+ *
  * Created on September 3, 2006, 5:34 PM
  *
  * To change this template, choose Tools | Template Manager
@@ -10,8 +18,11 @@
 package net.jnellis.binpack.packing;
 
 import net.jnellis.binpack.Bin;
+import net.jnellis.binpack.LinearBin;
 
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 /**
  * Places pieces into the emptiest bin first.
@@ -22,17 +33,14 @@ public class WorstFitPackingPolicy implements LinearPackingPolicy {
   @Override
   public List<Bin<Double>> pack(Double piece, List<Bin<Double>>
       existingBins, List<Double> availableCapacities) {
-    return null;
 
-//    pieces.forEach(piece -> {
-//      LinearBin<Double> emptiest = new BinImpl<Double>(capacity);
-//      // find the emptiest bin or create a new one.
-//      emptiest = bins.stream().min(emptiest::compare)
-//          .orElse(createNewBin(bins, capacity));
-//
-//      assert emptiest.canFit(piece);
-//      place(piece, emptiest);
-//    });
+    Bin<Double> emptiestBin = existingBins.stream()
+        .filter(bin -> bin.canFit(piece))
+        .max(comparing(LinearBin::getMaxRemainingCapacity))
+        .orElseGet(() -> addNewBin(existingBins, piece, availableCapacities));
+
+    emptiestBin.add(piece);
+    return existingBins;
   }
 
 
---
+++ src/test/groovy/net/jnellis/binpack/packing/WorstFitPackingPolicyTest.groovy
@@ -0,0 +1,48 @@
+/*
+ * WorstFitPackingPolicyTest.groovy
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.jnellis.binpack.packing
+
+import net.jnellis.binpack.Bin
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * User: Joe Nellis
+ * Date: 12/17/2014 
+ * Time: 5:56 PM
+ *
+ */
+class WorstFitPackingPolicyTest extends Specification {
+  @Shared
+  def bins = new ArrayList<Bin<Double>>();
+
+  @Unroll
+  def "Pack the emptiest bin or a new bin."() {
+    setup:
+    def policy = new WorstFitPackingPolicy()
+    def availableCapacities = [8d, 4d, 3d].asList()
+    expect:
+    bins == policy.pack(piece, bins, availableCapacities)
+    bins.collect {
+      it.getPieces()
+    } == result
+    where:
+    piece || result
+
+    8d    || [[8d]]
+    5d    || [[8d], [5d]]
+    5d    || [[8d], [5d], [5d]]
+    4d    || [[8d], [5d], [5d], [4d]]
+    3.6d  || [[8d], [5d], [5d], [4d, 3.6d]]
+    2.5d  || [[8d], [5d, 2.5d], [5d], [4d, 3.6d]]
+    2.3d  || [[8d], [5d, 2.5d], [5d, 2.3d], [4d, 3.6d]]
+    0.5d  || [[8d], [5d, 2.5d], [5d, 2.3d, 0.5d], [4d, 3.6d]]
+  }
+}
